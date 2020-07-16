e347b8e7d04c9490b09819631143e446cd1309f6
Syed Najam <syednajam997@gmail.com>
2020-07-16 21:22:34 +0200

    Added Project files

---
+++ AddApptrFramework/AddApptrFramework/AddApptrFramework.h
@@ -0,0 +1,19 @@
+//
+//  AddApptrFramework.h
+//  AddApptrFramework
+//
+//  Created by Syed Najam on 16/07/2020.
+//  Copyright © 2020 Syed Najam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AddApptrFramework.
+FOUNDATION_EXPORT double AddApptrFrameworkVersionNumber;
+
+//! Project version string for AddApptrFramework.
+FOUNDATION_EXPORT const unsigned char AddApptrFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AddApptrFramework/PublicHeader.h>
+
+
---
+++ AddApptrFramework/Pods/Target Support Files/Pods-AddApptrFramework-AddApptrFrameworkTests/Pods-AddApptrFramework-AddApptrFrameworkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AddApptrFramework_AddApptrFrameworkTests : NSObject
+@end
+@implementation PodsDummy_Pods_AddApptrFramework_AddApptrFrameworkTests
+@end
