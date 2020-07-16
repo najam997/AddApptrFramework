512a714ddfbc052664eadd22be3ac9686beee809
Syed Najam <syednajam997@gmail.com>
2020-07-17 00:57:31 +0200

    Updated Headerfile

--- AddApptrFramework/AddApptrFramework/AddApptrFramework.h
+++ AddApptrFramework/AddApptrFramework/AddApptrFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char AddApptrFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AddApptrFramework/PublicHeader.h>
 
-
+#import <AddApptrFramework/GitUsersRepos.h>
